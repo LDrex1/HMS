0ba9adf151eaee8cf370954cc991c54499252642
Edward-Precious Omegbu <eomegbu@gmail.com>
2022-07-18 03:33:27 +0100

    radial bar on dashboard done

--- js/dashboard.js
+++ js/dashboard.js
@@ -26,8 +26,11 @@ var options = {
     type: 'radialBar',
   },
   series: [70],
+  colors: ["#6C5DD3"],
   plotOptions: {
     radialBar: {
+      offsetX: 70,
+      offsetY: -12,
       hollow: {
         margin: 2,
         size: "30%"
@@ -44,12 +47,79 @@ var options = {
     }
   },
   responsive: [{
+    breakpoint: 1300,
+    options: {
+      plotOptions: {
+        radialBar: {
+          offsetX: 50,
+          offsetY: -12
+        }
+      }
+    }
+  },
+  {
+    breakpoint: 1150,
+    options: {
+      chart: {
+        height: 115,
+      },
+      plotOptions: {
+        radialBar: {
+          offsetY: 0,
+          hollow: {
+            size: "20%"
+          }
+        }
+      }
+    }
+  },
+  {
     breakpoint: 991.99,
     options: {
       chart: {
         height: 200,
+      },
+      plotOptions: {
+        radialBar: {
+          offsetX: 20,
+          offsetY: -29,
+          hollow: {
+            size: "30%"
+          }
+        }
       }
     }
+  },
+  {
+    breakpoint: 767.99,
+    options: {
+      plotOptions: {
+        radialBar: {
+          offsetX: 55,
+          offsetY: -29
+        }
+      }
+    }
+  },
+  {
+    breakpoint: 499.99,
+    options: {
+      plotOptions: {
+        radialBar: {
+          offsetX: 25
+        }
+      }
+    } 
+  },
+  {
+    breakpoint: 399.99,
+    options: {
+      plotOptions: {
+        radialBar: {
+          offsetX: 5
+        }
+      }
+    } 
   }],
   labels: ['Progress'],
 }
@@ -57,86 +127,345 @@ var options = {
   var chart = new ApexCharts(document.querySelector("#new-booking"), options);
   chart.render();
 
-//   var options = {
-//     series: [7, 3],
-//     chart: {
-//     type: 'donut',
-//     width: 60,
-// },
-//   dataLabels: {
-//     enabled: false
-// },
-//    responsive: [{
-//      breakpoint: 992,
-//      options: {
-//         chart: {
-//             width: 100,
-//         },
-//         dataLabels: {
-//             enabled: false
-//         },
-//         legend: {
-//          show: false
-//        }
-//      }
-//    }],
+  var options = {
+    chart: {
+      height: 150,
+      type: 'radialBar',
+    },
+    series: [65],
+    colors: ["#FD8539"],
+    plotOptions: {
+      radialBar: {
+        offsetX: 70,
+        offsetY: -12,
+        hollow: {
+          margin: 2,
+          size: "30%"
+        },
+        dataLabels: {
+          showOn: "always",
+          name: {
+            show: false
+          },
+          value: {
+            show: false
+          }
+        }
+      }
+    },
+    responsive: [{
+      breakpoint: 1300,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 50,
+            offsetY: -12
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 1150,
+      options: {
+        chart: {
+          height: 115,
+        },
+        plotOptions: {
+          radialBar: {
+            offsetY: 0,
+            hollow: {
+              size: "20%"
+            }
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 991.99,
+      options: {
+        chart: {
+          height: 200,
+        },
+        plotOptions: {
+          radialBar: {
+            offsetX: 20,
+            offsetY: -29,
+            hollow: {
+              size: "30%"
+            }
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 767.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 55,
+            offsetY: -29
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 499.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 25
+          }
+        }
+      } 
+    },
+    {
+      breakpoint: 399.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 5
+          }
+        }
+      } 
+    }],
+    labels: ['Progress'],
+  }
 
-//   };
+  var chartTwo = new ApexCharts(document.querySelector("#scheduled-rooms"), options);
+  chartTwo.render();
 
-//   var chartTwo = new ApexCharts(document.querySelector("#scheduled-rooms"), options);
-//   chartTwo.render();
+  var options = {
+    chart: {
+      height: 150,
+      type: 'radialBar',
+    },
+    series: [85],
+    colors: ["#2ED480"],
+    plotOptions: {
+      radialBar: {
+        offsetX: 70,
+        offsetY: -12,
+        hollow: {
+          margin: 2,
+          size: "30%"
+        },
+        dataLabels: {
+          showOn: "always",
+          name: {
+            show: false
+          },
+          value: {
+            show: false
+          }
+        }
+      }
+    },
+    responsive: [{
+      breakpoint: 1300,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 50,
+            offsetY: -12
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 1150,
+      options: {
+        chart: {
+          height: 115,
+        },
+        plotOptions: {
+          radialBar: {
+            offsetY: 0,
+            hollow: {
+              size: "20%"
+            }
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 991.99,
+      options: {
+        chart: {
+          height: 200,
+        },
+        plotOptions: {
+          radialBar: {
+            offsetX: 20,
+            offsetY: -29,
+            hollow: {
+              size: "30%"
+            }
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 767.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 55,
+            offsetY: -29
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 499.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 25
+          }
+        }
+      } 
+    },
+    {
+      breakpoint: 399.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 5
+          }
+        }
+      } 
+    }],
+    labels: ['Progress'],
+  }
 
-// var options = {
-//     series: [8, 2],
-//     chart: {
-//         type: 'donut',
-//         width: 60,
-//     },
-//     dataLabels: {
-//         enabled: false
-//     },
-//    responsive: [{
-//      breakpoint: 992,
-//      options: {
-//        chart: {
-//          width: 300
-//        },
-//        legend: {
-//          show: false
-//        }
-//      }
-//    }],
+  var chartThree = new ApexCharts(document.querySelector("#check-in"), options);
+  chartThree.render();
 
-// };
+  var options = {
+    chart: {
+      height: 150,
+      type: 'radialBar',
+    },
+    series: [55],
+    colors: ["#FE6D8E"],
+    plotOptions: {
+      radialBar: {
+        offsetX: 70,
+        offsetY: -12,
+        hollow: {
+          margin: 2,
+          size: "30%"
+        },
+        dataLabels: {
+          showOn: "always",
+          name: {
+            show: false
+          },
+          value: {
+            show: false
+          }
+        }
+      }
+    },
+    responsive: [{
+      breakpoint: 1300,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 50,
+            offsetY: -12
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 1150,
+      options: {
+        chart: {
+          height: 115,
+        },
+        plotOptions: {
+          radialBar: {
+            offsetY: 0,
+            hollow: {
+              size: "20%"
+            }
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 991.99,
+      options: {
+        chart: {
+          height: 200,
+        },
+        plotOptions: {
+          radialBar: {
+            offsetX: 20,
+            offsetY: -29,
+            hollow: {
+              size: "30%"
+            }
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 767.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 55,
+            offsetY: -29
+          }
+        }
+      }
+    },
+    {
+      breakpoint: 499.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 25
+          }
+        }
+      } 
+    },
+    {
+      breakpoint: 399.99,
+      options: {
+        plotOptions: {
+          radialBar: {
+            offsetX: 5
+          }
+        }
+      } 
+    }],
+    labels: ['Progress'],
+  }
 
-//   var chartThree = new ApexCharts(document.querySelector("#check-in"), options);
-//   chartThree.render();
+  var chartFour = new ApexCharts(document.querySelector("#check-out"), options);
+  chartFour.render();
 
-//   var options = {
-//     series: [9, 1],
-//     chart: {
-//     type: 'donut',
-//     width: 60,
-// },
-//   dataLabels: {
-//     enabled: false
-// },
-//    responsive: [{
-//      breakpoint: 992,
-//      options: {
-//        chart: {
-//          width: 100
-//        },
-//        dataLabels: {
-//     enabled: false
-// },
-//        legend: {
-//          show: false
-//        }
-//      }
-//    }],
-
-//   };
-
-//   var chartFour = new ApexCharts(document.querySelector("#check-out"), options);
-//   chartFour.render();
+  var options1 = {
+    chart: {
+      height: 280,
+      type: "radialBar",
+    },
+    series: [85, 90, 70, 80, 70],
+    plotOptions: {
+      radialBar: {
+        dataLabels: {
+          total: {
+            show: true,
+            label: 'TOTAL'
+          }
+        }
+      }
+    },
+    labels: ['TEAM A', 'TEAM B', 'TEAM C', 'TEAM D', 'TEAM E']
+  };
+  
+  new ApexCharts(document.querySelector("#referrals"), options1).render();
+  
